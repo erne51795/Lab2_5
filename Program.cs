cd083ca8131091f9281f2d710d98d0dd05ce6b74
erne51795 <66523436+erne51795@users.noreply.github.com>
2020-08-07 09:41:20 -0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -34,8 +34,36 @@ namespace Lab2_5
             Console.WriteLine(results3);
 
 
+            string name = "Krystal";
+            var date = DateTime.Now;
+            Console.WriteLine("Hello, {0}! Today is {1}, it's {2:HH:mm} now", name, date.DayOfWeek, date);
+
+            Console.WriteLine($"Hello,{name}! Today is {date.DayOfWeek}, it's {date:HH:mm} now.");
+
+            int age;
+            Console.Write("Enter your age ");
+            age = Convert.ToInt32(Console.ReadLine());
+            Console.Write("You look younger than {0} ", age);
+
+
+            int num1, num2, num3;
+
+            Console.Write("Input the first number to multiply: ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Input the second number to multiply: ");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Input the third number to multiply: ");
+            num3 = Convert.ToInt32(Console.ReadLine());
+
+            int result = num1 * num2 * num3;
+            Console.WriteLine("Output: {0} x {1} x {2} = {3}",
+                                    num1, num2, num3, result);
+
 
 
         }
     }
 }
+
